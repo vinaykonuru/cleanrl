e2add5ca9f794e1a8b34fe3ddd6b379032d78b25
Rajiv Swamy <rswamy@princeton.edu>
2024-04-21 10:02:43 -0400

    added device print outs

--- cleanrl/ddpg_continuous_action.py
+++ cleanrl/ddpg_continuous_action.py
@@ -153,6 +153,8 @@ poetry run pip install "stable_baselines3==2.0.0a1"
 
     device = torch.device("cuda" if torch.cuda.is_available() and args.cuda else "cpu")
 
+    print(f"Device: {device}")
+
     # env setup
     envs = gym.vector.SyncVectorEnv([make_env(args.env_id, args.seed, 0, args.capture_video, run_name)])
     assert isinstance(envs.single_action_space, gym.spaces.Box), "only continuous action space is supported"
